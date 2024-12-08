eb209447b87abcddbc09580329e9fa84c6e8599a
ej-edades <2223203@slu.edu.ph>
2024-12-08 23:31:10 +0800

    remove delete function

--- src/student_library.c
+++ src/student_library.c
@@ -5,7 +5,8 @@
 #define MAX_STUDENTS 100
 #define NAME_LENGTH 50
 
-typedef struct {
+typedef struct
+{
     int id;
     char name[NAME_LENGTH];
     float grade;
@@ -14,8 +15,10 @@ typedef struct {
 Student students[MAX_STUDENTS];
 int studentCount = 0;
 
-void addStudent() {
-    if (studentCount >= MAX_STUDENTS) {
+void addStudent()
+{
+    if (studentCount >= MAX_STUDENTS)
+    {
         printf("Cannot add more students. The list is full.\n");
         return;
     }
@@ -31,31 +34,16 @@ void addStudent() {
     printf("Student added successfully.\n");
 }
 
-void removeStudent() {
-    int id;
-    printf("Enter Student ID to remove: ");
-    scanf("%d", &id);
-
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
-            for (int j = i; j < studentCount - 1; j++) {
-                students[j] = students[j + 1];
-            }
-            studentCount--;
-            printf("Student removed successfully.\n");
-            return;
-        }
-    }
-    printf("Student with ID %d not found.\n", id);
-}
-
-void updateStudentDetails() {
+void updateStudentDetails()
+{
     int id;
     printf("Enter Student ID to update: ");
     scanf("%d", &id);
 
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
+    for (int i = 0; i < studentCount; i++)
+    {
+        if (students[i].id == id)
+        {
             printf("Enter new name for the student: ");
             scanf(" %[^\n]s", students[i].name);
             printf("Student details updated successfully.\n");
@@ -65,13 +53,16 @@ void updateStudentDetails() {
     printf("Student with ID %d not found.\n", id);
 }
 
-void searchStudent() {
+void searchStudent()
+{
     int id;
     printf("Enter Student ID to search: ");
     scanf("%d", &id);
 
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
+    for (int i = 0; i < studentCount; i++)
+    {
+        if (students[i].id == id)
+        {
             printf("Student ID: %d\nName: %s\nGrade: %.2f\n",
                    students[i].id, students[i].name,
                    students[i].grade == -1 ? 0 : students[i].grade);
@@ -81,27 +72,33 @@ void searchStudent() {
     printf("Student with ID %d not found.\n", id);
 }
 
-void listAllStudents() {
-    if (studentCount == 0) {
+void listAllStudents()
+{
+    if (studentCount == 0)
+    {
         printf("No students to display.\n");
         return;
     }
 
     printf("List of All Students:\n");
-    for (int i = 0; i < studentCount; i++) {
+    for (int i = 0; i < studentCount; i++)
+    {
         printf("ID: %d, Name: %s, Grade: %.2f\n",
                students[i].id, students[i].name,
                students[i].grade == -1 ? 0 : students[i].grade);
     }
 }
 
-void viewStudentGrade() {
+void viewStudentGrade()
+{
     int id;
     printf("Enter Student ID to view grade: ");
     scanf("%d", &id);
 
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
+    for (int i = 0; i < studentCount; i++)
+    {
+        if (students[i].id == id)
+        {
             printf("Grade for %s (ID: %d): %.2f\n",
                    students[i].name, students[i].id,
                    students[i].grade == -1 ? 0 : students[i].grade);
@@ -111,13 +108,16 @@ void viewStudentGrade() {
     printf("Student with ID %d not found.\n", id);
 }
 
-void setStudentGrade() {
+void setStudentGrade()
+{
     int id;
     printf("Enter Student ID to set grade: ");
     scanf("%d", &id);
 
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
+    for (int i = 0; i < studentCount; i++)
+    {
+        if (students[i].id == id)
+        {
             printf("Enter grade for %s: ", students[i].name);
             scanf("%f", &students[i].grade);
             printf("Grade set successfully.\n");
